e2de7c40acfc6741e940f04b71e8381fa9a7ab56
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-12-23 20:25:36 +0530

    Game-setup

---
+++ Key Code/script.js
@@ -0,0 +1,8 @@
+const key = document.getElementById('key');
+const number = document.getElementById('num');
+const digit = document.getElementById('digi');
+
+
+const myFun = onkeyup(()=> {
+    alert("fuck you");
+})
