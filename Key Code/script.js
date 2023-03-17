600d84d1f690c74c0b6756d567a9bc94648487a2
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-03-17 11:08:29 +0530

    Expanding Cards Dom Project

--- Key Code/script.js
+++
@@ -1,8 +0,0 @@
-const key = document.getElementById('key');
-const number = document.getElementById('num');
-const digit = document.getElementById('digi');
-
-
-const myFun = onkeyup(()=> {
-    alert("fuck you");
-})
